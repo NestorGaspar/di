d3f26c6fef985e54d95c6d2518d52cc6ec593bd9
NestorGaspar <nestorgsg@gmail.com>
2021-06-15 14:34:23 +0200

    ah

--- src/sample/Controller3.java
+++ src/sample/Controller3.java
@@ -1,5 +1,8 @@
 package sample;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Controller3 {
 
     Controller controller1 = null;
@@ -7,4 +10,54 @@ public class Controller3 {
     public void informacionEntreVentanas(Controller Controller){
         controller1 = Controller;
     }
+
+    public void actualizacionFechal1(){
+
+        Long tiempoA = System.currentTimeMillis();
+        SimpleDateFormat formatoF = new SimpleDateFormat("dd-MM-YYYY kk:mm");
+        Date fecha = new Date(tiempoA);
+        String tiempo = formatoF.format(fecha);
+        controller1.label1.setText("" + tiempo);
+    }
+    public void actualizacionFechal2(){
+
+        Long tiempoA = System.currentTimeMillis();
+        SimpleDateFormat formatoF = new SimpleDateFormat("dd-MM-YYYY kk:mm");
+        Date fecha = new Date(tiempoA);
+        String tiempo = formatoF.format(fecha);
+        controller1.label2.setText("" + tiempo);
+    }
+    public void actualizacionFechal3(){
+
+        Long tiempoA = System.currentTimeMillis();
+        SimpleDateFormat formatoF = new SimpleDateFormat("dd-MM-YYYY kk:mm");
+        Date fecha = new Date(tiempoA);
+        String tiempo = formatoF.format(fecha);
+        controller1.label3.setText("" + tiempo);
+    }
+    public void actualizacionFechal4(){
+
+        Long tiempoA = System.currentTimeMillis();
+        SimpleDateFormat formatoF = new SimpleDateFormat("dd-MM-YYYY kk:mm");
+        Date fecha = new Date(tiempoA);
+        String tiempo = formatoF.format(fecha);
+        controller1.label4.setText("" + tiempo);
+    }
+    public void actualizacionFechal5(){
+
+        Long tiempoA = System.currentTimeMillis();
+        SimpleDateFormat formatoF = new SimpleDateFormat("dd-MM-YYYY kk:mm");
+        Date fecha = new Date(tiempoA);
+        String tiempo = formatoF.format(fecha);
+        controller1.label5.setText("" + tiempo);
+    }
+    public void actualizacionFechal6(){
+
+        Long tiempoA = System.currentTimeMillis();
+        SimpleDateFormat formatoF = new SimpleDateFormat("dd-MM-YYYY kk:mm");
+        Date fecha = new Date(tiempoA);
+        String tiempo = formatoF.format(fecha);
+        controller1.label6.setText("" + tiempo);
+    }
+
 }
--- src/sample/Juego.java
+++ src/sample/Juego.java
@@ -1,2 +1,16 @@
-package sample;public class Juego {
+package sample;
+
+public class Juego {
+    public boolean comprado = false;
+    public String nombre;
+    public String imagePath;
+
+
+    public Juego(boolean comprado, String nombre, String imagePath){
+        this.comprado = comprado;
+        this.nombre = nombre;
+        this.imagePath = imagePath;
+    }
+
+
 }
